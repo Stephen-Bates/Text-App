d2fe62ec953a279c40abd3a4f2f7d53397e2d275
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-02 15:27:41 -0400

    fixed database get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,24 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result[0].value', result[0]?.value);
+  return result[0]?.value;
 };
+
 initdb();
